46ef4ad99b5366ab68abc76c2ea14dd785a9029c
Eiichiro Iriguchi <eiichiro_iriguchi@a-1ro.dev>
2026-05-04 01:11:33 +0900

    style: fix biome format in nanoka.config.ts

--- packages/create-nanoka-app/templates/default/nanoka.config.ts
+++ packages/create-nanoka-app/templates/default/nanoka.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from '@nanokajs/core/config'
 import { postFields, postTableName } from './src/models/posts'
 
 export default defineConfig({
-  models: [
-    { name: postTableName, fields: postFields },
-  ],
+  models: [{ name: postTableName, fields: postFields }],
   output: './drizzle/schema.ts',
 })
